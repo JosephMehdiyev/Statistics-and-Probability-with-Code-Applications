99ae7b3f495c6b8b99a6991af70ec20a776bb9a2
Yusif Mehdiyev <yusifmehdiyev55@gmail.com>
2024-06-24 08:27:17 +0400

    Chapter 1 Question 2

--- src/chapter1/1.py
+++ src/chapter1/1.py
@@ -9,20 +9,20 @@ p3 = 0.07
 numberOfExperiments = n
 
 # We already discussed about binomial distributions. Coin flipping is one of these.
-# This function does the experiment size times, saves them in result variable as an array.
+# This function does the experiment n times, saves them in result variable as an array.
 result1 = np.random.binomial(n = n,p = p1, size = numberOfExperiments)
 result2 = np.random.binomial(n = n,p = p2, size = numberOfExperiments)
 result3 = np.random.binomial(n = n,p = p3, size = numberOfExperiments)
 
 
 #
-nn = np.arange(1, n + 1)
-plt.figure(figsize=(4.5, 3.5))
+nn = np.arange(0, n, 1)
+plt.figure(figsize=(6.5, 3.5))
 plt.plot(nn, np.cumsum(result1) / (nn*n), label='p = 0.3')
 plt.plot(nn, np.cumsum(result2) / (nn*n), label='p = 0.5')
-plt.plot(nn, np.cumsum(result3) / (nn*n), label='p = 0.3')
+plt.plot(nn, np.cumsum(result3) / (nn*n), label='p = 0.07')
 
 plt.legend(loc='upper right')
-plt.ylabel(" Head to toss ratio")
+plt.ylabel("Average ratio")
 plt.xlabel("Number of Experiments")
 plt.savefig('src/chapter1/fig1.pgf')
---
+++ src/chapter1/2.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n = 100
+numberOfExperiments = n
+# This is a numpy thing. It will be used for generating probability distributions.
+# Basically makes the probability possible
+rng = np.random.default_rng()
+
+# Here we draw random integers in interval (1,6) for n times, and repeat the process "numberOfExperiment" times 
+result = rng.integers(low = 1, high = 7, size = (n, numberOfExperiments) )
+
+# This loop counts the number of capA we get in one experiment, and loops through the total experiments.
+totalCapA = []
+for element in result:
+    # Outputs Bool values depending on the input array, i.e [2,4,6]
+    initialCapA = np.isin(element, [2,4,6])
+    # Number of Bool values.
+    totalCapA.append(initialCapA.sum())
+
+# Repeat the same process for capB and capAB
+totalCapB = []
+for element in result:
+    initialCapB = np.isin(element, [1,2,3,4])
+    totalCapB.append(initialCapB.sum())
+totalCapAB = []
+for element in result:
+    initialCapAB = np.isin(element, [2,4])
+    totalCapAB.append(initialCapAB.sum())
+
+# A simple array [1,2,...,n]
+experimentOrder = np.arange(1,n+1,1)
+
+
+plt.figure(figsize=(6.5, 3.5))
+
+# The fancy expressions here just takes the average value. 
+# X axis is "experimentOrder" while the Y axis is our fancy expressions.
+plt.plot(experimentOrder, np.cumsum(totalCapA)/(experimentOrder *n), label = "$\widehat{P}(A)$")
+plt.plot(experimentOrder, np.cumsum(totalCapB)/(experimentOrder *n), label = "$\widehat{P}(B)$")
+plt.plot(experimentOrder, np.cumsum(totalCapAB)/(experimentOrder *n), label = "$\widehat{P}(AB)$")
+plt.plot(experimentOrder, np.multiply(np.cumsum(totalCapA),np.cumsum(totalCapB))/((experimentOrder *n)*(experimentOrder *n)), label = "$\widehat{P}(A)\widehat{P}(B)$")
+
+plt.ylabel("Average ratio")
+plt.xlabel("Number of Experiments")
+plt.legend(loc='upper right')
+plt.savefig('src/chapter1/fig2.pgf')
